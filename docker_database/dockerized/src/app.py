1aec47922981697fd6273abf47e80ea0539f4d6c
Tim Kauer <tim.kauer@sap.com>
2020-05-21 15:14:45 +0200

    removed unnecessary folders + added sql query + Player

--- docker_database/dockerized/src/app.py
+++ docker_database/dockerized/src/app.py
@@ -3,12 +3,12 @@ import json
 from flask import request, render_template, redirect, flash, url_for
 
 from . import create_app, database
-from .models import Soccer, User, db
+from .models import db, Soccer, User, Player
 
 app = create_app()
-app.debug = True
+app.debug = True #receive better error messages
 
-
+# Test database connection
 @app.route('/', methods=['GET'])
 def fetch():
     cats = database.get_all(Soccer)
@@ -24,6 +24,15 @@ def fetch():
         all_cats.append(new_cat)
     return json.dumps(all_cats), 200
 
+@app.route('/run', methods=['GET'])
+def run():
+    # session = Session()
+    all_users = []
+    userss = db.session.query(User).filter_by(name="Success").all()
+    for u in userss:
+        all_users.append(u.id)
+    # session.close()
+    return render_template('show_user.html', user=all_users)
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -60,7 +69,7 @@ def Index():
 def insert():
  
     if request.method == 'POST':
- 
+        
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
@@ -98,3 +107,61 @@ def delete(id):
     flash("Employee Deleted Successfully")
  
     return redirect(url_for('Index'))
+
+# query on all our employee data
+a = '/player'
+@app.route(a)
+def Players():
+    all_data = Player.query.all()
+
+    return render_template("player.html", employees=all_data)
+
+@app.route('/insert_player', methods=['POST'])
+def insert_player():
+ 
+    if request.method == 'POST':
+        
+        fname = request.form['fname']
+        lname = request.form['lname']
+        pl_no = request.form['pl_no']
+        nationality = request.form['nationality']
+        pl_goals = request.form['pl_goals']
+     
+ 
+        my_data = Player(fname, lname, pl_no, nationality, pl_goals)
+        db.session.add(my_data)
+        db.session.commit()
+ 
+        #database.add_instance(model=Soccer, name, email, phone)
+        flash("Player Inserted Successfully")
+        return redirect(url_for('Players'))
+
+#this is our update route where we are going to update our employee
+@app.route('/update_player', methods = ['GET', 'POST'])
+def update_player():
+ 
+    if request.method == 'POST':
+        my_data = Player.query.get(request.form.get('id'))
+        #fname, lname, pl_no, nationality, 
+        
+        my_data.fname = request.form['fname']
+        my_data.lname = request.form['lname']
+        my_data.pl_no = request.form['pl_no']
+        my_data.nationality = request.form['nationality']
+        my_data.pl_goals = request.form['pl_goals']
+
+ 
+        db.session.commit()
+        flash("Player Updated Successfully")
+ 
+        return redirect(url_for('Players'))
+
+#This route is for deleting our employee
+@app.route('/delete_player/<id>/', methods = ['GET', 'POST'])
+def delete_player(id):
+    my_data = Player.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Player Deleted Successfully")
+ 
+    return redirect(url_for('Players'))
--- docker_database/dockerized/src/models.py
+++ docker_database/dockerized/src/models.py
@@ -27,19 +27,22 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.name)
 
-# class Player(db.Model):
-#     __tablename__ = "players"
+class Player(db.Model):
+    __tablename__ = "players"
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     fname = db.Column(db.String(128), unique=True, nullable=False)
-#     lname = db.Column(db.String(100))
-#     pl_no = db.Column(db.Integer)
-#     nationality = db.Column(db.String(100))
-#     pl_goals = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(128), unique=True, nullable=False)
+    lname = db.Column(db.String(100))
+    pl_no = db.Column(db.Integer)
+    nationality = db.Column(db.String(100))
+    pl_goals = db.Column(db.Integer)
 
-    # def __init__(self, fname, lname, pl_no, nationality, pl_goals):
-    #     self.fname = fname
-    #     self.lname = lname
-    #     self.pl_no = pl_no
-    #     self.nationality = nationality
-    #     self.pl_goals = pl_goals
+    def __init__(self, fname, lname, pl_no, nationality, pl_goals):
+        self.fname = fname
+        self.lname = lname
+        self.pl_no = pl_no
+        self.nationality = nationality
+        self.pl_goals = pl_goals
+    
+    def __repr__(self):
+        return '<Player %r>' % (self.name)
